c9af3916c714ed2a40f5ce8b36426d8ce691cd39
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-04-16 00:28:04 +0200

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
-    id("com.yausername.youtubedl_android")
-    id("signing")
+    //id("com.yausername.youtubedl_android")
+    //id("signing")
     id("com.android.library")
-    id("maven-publish")
+    //id("maven-publish")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.yausername.youtubedl_common"
-    compileSdk = 34
+    //compileSdk = 34
 
     defaultConfig {
         minSdk = 24
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        //testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -25,20 +25,24 @@ android {
     }
 }
 
+/*
 configurePublishing {
     artifactId = project.name
     isPublished = true
 }
+*/
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
     implementation("androidx.appcompat:appcompat:${rootProject.extra["appCompatVer"]}")
     implementation("androidx.core:core-ktx:${rootProject.extra["coreKtxVer"]}")
+    implementation("commons-io:commons-io:${rootProject.extra["commonsIoVer"]}")
+    implementation("org.apache.commons:commons-compress:${rootProject.extra["commonsCompressVer"]}")
+
+    /*
     testImplementation("junit:junit:${rootProject.extra["junitVer"]}")
     androidTestImplementation("androidx.test.ext:junit:${rootProject.extra["androidJunitVer"]}")
     androidTestImplementation("androidx.test.espresso:espresso-core:${rootProject.extra["espressoVer"]}")
-
-    implementation("commons-io:commons-io:${rootProject.extra["commonsIoVer"]}")
-    implementation("org.apache.commons:commons-compress:${rootProject.extra["commonsCompressVer"]}")
+    */
 }
