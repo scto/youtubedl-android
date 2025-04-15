48535d1858551b21ed50e997c429d581c3193c8d
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-04-16 00:31:32 +0200

    Update build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    id("com.yausername.youtubedl_android")
-    id("signing")
+    //id("com.yausername.youtubedl_android")
+    //id("signing")
     id("com.android.library")
-    id("maven-publish")
+    //id("maven-publish")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.yausername.youtubedl_android"
-    compileSdk = 34
+    //compileSdk = 34
 
     defaultConfig {
         minSdk = 24
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        //testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -27,23 +27,24 @@ android {
     }
 }
 
+/*
 configurePublishing {
     artifactId = project.name
     isPublished = true
 }
+*/
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-
     implementation(project(":common"))
-
     implementation("androidx.appcompat:appcompat:${rootProject.extra["appCompatVer"]}")
     implementation("androidx.core:core-ktx:${rootProject.extra["coreKtxVer"]}")
-    testImplementation("junit:junit:${rootProject.extra["junitVer"]}")
-    androidTestImplementation("androidx.test.ext:junit:${rootProject.extra["androidJunitVer"]}")
-    androidTestImplementation("androidx.test.espresso:espresso-core:${rootProject.extra["espressoVer"]}")
-
     implementation("com.fasterxml.jackson.core:jackson-databind:${rootProject.extra["jacksonVer"]}")
     implementation("com.fasterxml.jackson.core:jackson-annotations:${rootProject.extra["jacksonVer"]}")
     implementation("commons-io:commons-io:${rootProject.extra["commonsIoVer"]}")
+    /*
+    testImplementation("junit:junit:${rootProject.extra["junitVer"]}")
+    androidTestImplementation("androidx.test.ext:junit:${rootProject.extra["androidJunitVer"]}")
+    androidTestImplementation("androidx.test.espresso:espresso-core:${rootProject.extra["espressoVer"]}")
+    */
 }
