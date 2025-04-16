1bba76ee626a489c7a4bfdb70642dc4582b85165
scto <tschmid35@gmail.com>
2025-04-16 02:25:24 +0200

    feat: some github workflow additions, gradle fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import com.android.build.api.dsl.Publishing
-//import com.android.build.gradle.internal.cxx.os.exe
-
 import com.android.build.gradle.BaseExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
@@ -11,61 +8,6 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin) apply false
 }
-
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-/*
-buildscript {
-    //val kotlin_version by extra("1.7.22")
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            isAllowInsecureProtocol = true
-            url = uri("http://jcenter.bintray.com")
-        }
-
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-*/
-
-/*
-val versionMajor = 0
-val versionMinor = 17
-val versionPatch = 3
-val versionBuild = 0 // bump for dogfood builds, public betas, etc.
-val versionCode = versionMajor * 100000 + versionMinor * 1000 + versionPatch * 100 + versionBuild
-val versionName = "$versionMajor.$versionMinor.$versionPatch"
-
-extra.apply {
-    set("versionCode", versionCode)
-    set("versionName", "$versionMajor.$versionMinor.$versionPatch")
-    set("appCompatVer", "1.4.2")
-    set("junitVer", "4.13.2")
-    set("androidJunitVer", "1.1.3")
-    set("espressoVer", "3.4.0")
-    set("jacksonVer", "2.11.1")
-    set("commonsIoVer", "2.19.0") // supports java 17 //2.19.0
-    set("commonsCompressVer", "1.27.1") // supports java 17 //1.27.1
-    //set("commonsIoVer", "2.5") // supports java 1.6 //2.19.0
-    //set("commonsCompressVer", "1.12") // supports java 1.6 //1.27.1
-    set("coreKtxVer", "1.8.0")
-}
-*/
-
-/*
-allprojects {
-    group = "com.github.yausername"
-    version = versionName
-}
-*/
 
 fun Project.configureBaseExtension() {
   extensions.findByType(BaseExtension::class)?.run {
@@ -86,6 +28,9 @@ fun Project.configureBaseExtension() {
 }
 
 subprojects {
+    plugins.withId("com.android.application") { configureBaseExtension() }
+    plugins.withId("com.android.library") { configureBaseExtension() }
+
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
             apiVersion = KotlinVersion.KOTLIN_2_0
@@ -100,26 +45,4 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 
-/*
-tasks.register("packagePublishedArtifacts") {
-    val librariesToPublish = listOf("common", "library", "aria2c", "ffmpeg")
-    librariesToPublish.forEach {
-        dependsOn(":$it:publishReleasePublicationToMavenRepository")
-    }
-    doLast {
-        exec {
-            workingDir = project.buildDir.resolve("staging-deploy")
-            standardOutput = System.out
-            errorOutput = System.err
-
-            val zipCommands = listOf(
-                "zip",
-                "-r",
-                project.buildDir.resolve("archive-$versionName.zip").absolutePath,
-            ) + librariesToPublish.map { "io/github/junkfood02/youtubedl-android/$it/$versionName" }
-
-            commandLine(zipCommands)
-        }
-    }
-}
-*/
+tasks.register<Delete>("clean") { delete(rootProject.layout.buildDirectory) }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,29 @@
-include(":common", ":app", ":library", ":ffmpeg", ":aria2c")
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+    maven("https://cache-redirector.jetbrains.com/kotlin.bintray.com/kotlin-plugin")
+  }
+}
+
+//plugins {
+//  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+//}
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        jcenter()
-        // e.g this is how you would add jitpack
-        maven("https://jitpack.io")
-        // Add any repositories you would be adding to all projects here
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+    maven { url = uri("https://jitpack.io") }
+  }
 }
+
+rootProject.name = "YouTubeDownloader"
+
+include(":common", ":app", ":library", ":ffmpeg", ":aria2c")
